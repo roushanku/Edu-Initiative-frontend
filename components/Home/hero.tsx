13def5103ca7ee3bd53486ad3ba2116be5c11b10
Mohd Adil <adil7k777@gmail.com>
2024-10-14 16:56:21 +0530

    changed hero

--- components/Home/hero.tsx
+++ components/Home/hero.tsx
@@ -1,58 +1,25 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-
-function HeroSection(){
+function HeroSection() {
     return (
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-        <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
+      <section className="flex flex-col items-center justify-center py-8 md:py-10 relative w-full h-[60vh] overflow-hidden">
+        <div className="w-full h-full bg-[url('https://images.pexels.com/photos/159844/cellular-education-classroom-159844.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover bg-center opacity-60"></div>
+  
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center z-10">
+          <div className="text-center text-2xl md:text-3xl font-semibold text-white p-4">
+            Empowering education for every child—connecting dedicated home tutors.
+          </div>
+  
+          <div className="flex w-full justify-center mt-5">
+            <button className="bg-sky-600 text-white border-solid border-medium border-sky-600 rounded-md px-5 py-3 m-3 hover:text-sky-600 hover:bg-white shadow-sm">
+              Hire Tutor
+            </button>
+            <button className="bg-sky-600 text-white border-solid border-medium border-sky-600 rounded-md px-5 py-3 m-3 hover:text-sky-600 hover:bg-white shadow-sm">
+              Show More
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
-    </section>
+      </section>
     );
-}
-
-export default HeroSection;
+  }
+  
+  export default HeroSection;
+  
