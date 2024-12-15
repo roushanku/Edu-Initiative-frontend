3efebe8a5a91696a8e19bfa0edb843d19cae9263
roushanku <roushanku@iitbhilai.ac.in>
2024-12-16 01:08:15 +0530

    feat : signUp page for User

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,9 +1,9 @@
-import SignInSide from "@/components/sign-in-side/SignInSide";
+import SignUpSide from "@/components/sign-in-side/SignUpSide";
 
 export default function SingIn() {
   return (
     <div className="">
-      <SignInSide />
+      <SignUpSide/>
     </div>
   );
 }
---
+++ components/sign-in-side/SignUpSide.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import SignUpCard from "./SignUpCard";
+import { title } from "../Primitives";
+import { Sign } from "crypto";
+
+const SignUpSide = () => {
+  const [mode, setMode] = useState<"light" | "dark">("light");
+
+//   useEffect(() => {
+//     const savedMode = localStorage.getItem("themeMode") as
+//       | "light"
+//       | "dark"
+//       | null;
+//     if (savedMode) {
+//       setMode(savedMode);
+//     } else {
+//       const systemPrefersDark = window.matchMedia(
+//         "(prefers-color-scheme: dark)"
+//       ).matches;
+//       setMode(systemPrefersDark ? "dark" : "light");
+//     }
+//   }, []);
+
+  return (
+    <div
+      className={`flex flex-col w-5xl justify-between h-screen ${mode === "dark" ? "bg-gray-900 text-white" : "bg-white text-black"}`}
+    >
+      <main className="flex flex-col md:flex-col justify-center items-center gap-8 p-6">
+        <h1 className={title()}>Sign Up</h1>
+        <SignUpCard />
+      </main>
+      <footer className="p-4 text-center border-t">
+        &copy; {new Date().getFullYear()} My Application
+      </footer>
+    </div>
+  );
+};
+
+export default SignUpSide;
